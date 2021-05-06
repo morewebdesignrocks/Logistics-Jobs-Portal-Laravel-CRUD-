5c253405e4de23eff3ff195c06e90875051f913b
Andres Robayo <morewebdesignrocks@gmail.com>
2021-05-06 12:10:57 -0400

    Add select fields to create form

--- welcome.blade.php
+++ welcome.blade.php
@@ -18,20 +18,28 @@
       </div>
     @endif
       <form method="post" action="{{ route('jobs.store') }}">
+        <div>
           <div class="form-group">
               @csrf
-              <label for="job_number">Job #:</label>
+              <label for="job_number">Job Number:</label>
               <input type="number" class="form-control" name="job_number"/>
           </div>
           <div class="form-group">
-              <label for="job_type">Job Type</label>
-              <input type="text" class="form-control" name="job_type"/>
+              <label for="job_type">Job Type:</label>
+              <select type="text" class="form-control" name="job_type">
+                <option value="Wholesale">Wholesale</option>
+                <option value="Retail">Retail</option>
+                <option value="Inventory">Inventory</option>
+              </select>
           </div>
           <div class="form-group">
-              <label for="modality">Modality</label>
-              <input type="text" class="form-control" name="modality"/>
+              <label for="modality">Modality:</label>
+              <select type="text" class="form-control" name="modality">
+                <option value="C-Arm">C-Arm</option>
+              </select>
           </div>
-          <button type="submit" class="btn btn-block btn-primary">Add</button>
+        </div>
+        <button type="submit" class="btn btn-block btn-primary">Add</button>
       </form>
   </div>
 </div>
