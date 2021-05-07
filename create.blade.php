a3777a057ffb1aa26b4b1b74b913551ace43aaa5
Andres Robayo <morewebdesignrocks@gmail.com>
2021-05-07 10:17:42 -0400

    Add form style

--- create.blade.php
+++ create.blade.php
@@ -4,7 +4,7 @@
 
 <div class="card mt-5">
     <div class="card-header">
-        Create New Job
+        Add New Job
     </div>
 
     <div class="card-body">
@@ -18,38 +18,47 @@
         </div>
         @endif
         <form method="post" action="{{ route('jobs.store') }}">
-            <div>
-                <div class="form-group">
-                    @csrf
-                    <label for="job_number">Job Number:</label>
-                    <input type="number" class="form-control" name="job_number"/>
+            @csrf
+            <section class="">
+                <h3>Equipment Information</h3>
+                <fieldset class="row">
+                    <div class="form-group col">
+                        <label for="job_number">Job Number:</label>
+                        <input type="number" class="form-control" name="job_number"/>
+                    </div>
+                    <div class="form-group col">
+                        <label for="job_type">Job Type:</label>
+                        <select type="text" class="form-control" name="job_type">
+                            <option value="Wholesale">Wholesale</option>
+                            <option value="Retail">Retail</option>
+                            <option value="Inventory">Inventory</option>
+                        </select>
+                    </div>
+                    <div class="form-group col">
+                        <label for="modality">Modality:</label>
+                        <select type="text" class="form-control" name="modality">
+                            <option value="C-Arm">C-Arm</option>
+                        </select>
+                    </div>
+                </fieldset>
+                <fieldset class="row">
+                    <div class="form-group col">
+                        <label for="equipment_manufacturer">Equipment Manufacturer:</label>
+                        <input type="text" class="form-control" name="equipment_manufacturer"/>
+                        </select>
+                    </div>
+                    <div class="form-group col">
+                        <label for="modality">Equipment Model:</label>
+                        <input type="text" class="form-control" name="equipment_model"/>
+                        </select>
+                    </div>
+                </fieldset>
+            </section>
+            <fieldset class="row">
+                <div class="form-group col">
+                <button type="submit" class="btn btn-primary">Add</button>
                 </div>
-                <div class="form-group">
-                    <label for="job_type">Job Type:</label>
-                    <select type="text" class="form-control" name="job_type">
-                        <option value="Wholesale">Wholesale</option>
-                        <option value="Retail">Retail</option>
-                        <option value="Inventory">Inventory</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="modality">Modality:</label>
-                    <select type="text" class="form-control" name="modality">
-                        <option value="C-Arm">C-Arm</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="equipment_manufacturer">Equipment Manufacturer:</label>
-                    <input type="text" class="form-control" name="equipment_manufacturer"/>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="modality">Equipment Model:</label>
-                    <input type="text" class="form-control" name="equipment_model"/>
-                    </select>
-                </div>
-            </div>
-            <button type="submit" class="btn btn-block btn-primary">Add</button>
+            </fieldset>
         </form>
     </div>
 </div>
