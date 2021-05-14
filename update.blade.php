0ff3abda7ea2de2cb2bf2c39eafbccbddc12e8c1
Andres Robayo <morewebdesignrocks@gmail.com>
2021-05-14 10:51:06 -0400

    Add title and remove spacing

--- update.blade.php
+++ update.blade.php
@@ -3,9 +3,8 @@
 @section('content')
 <div class="card mt-5">
     <div class="card-header">
-        Update
+        <h3>Update Job</h3>
     </div>
-
     <div class="card-body">
         @if ($errors->any())
         <div class="alert alert-danger">
